55d6a2fd44d1969183525926861bfa699cfe7c9d
Callum Bloomfield <callum.bloomfield01@gmail.com>
2021-02-18 12:24:16 +0000

    Add datetime package

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '@/plugins/event-bus.ts',
+    '@/plugins/datepicker.ts',
     '@/plugins/axios-accessor.ts',
     '@/plugins/service-injector.ts',
   ],
