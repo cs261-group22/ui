0efcf5e6508d03a562882eb92b11e3dd0086d76e
Callum Bloomfield <callum.bloomfield01@gmail.com>
2021-02-18 12:29:00 +0000

    Add event buider master page

--- pages/dashboard/events/_id/modify.vue
+++ pages/dashboard/events/_id/modify.vue
@@ -1,31 +1,98 @@
 <template>
-  <div>
-    <header class="bg-white shadow">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between w-full">
-        <h1 class="text-3xl font-bold leading-tight text-primary">Modify Event Questions</h1>
+  <div class="flex w-full">
+    <div class="sidebar bg-primarydark">
+      <div class="sidebar-header h-24 lg:h-20 px-6 lg:px-8">
+        <p class="font-medium text-white">{{ truncatedEventName }}</p>
+
+        <p class="text-xs text-grey-500">Draft Event</p>
       </div>
-    </header>
-    <main>
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <div class="px-4 py-6 sm:px-0">
-          <!-- todo -->
-        </div>
+
+      <div class="sidebar-content px-6 py-4 lg:px-8">
+        <ul class="sidebar-nav list-reset">
+          <li :class="{ active: eventTab === EventTab.SETTINGS }">
+            <router-link :to="`?tab=${EventTab.SETTINGS}`"> Settings </router-link>
+          </li>
+
+          <li :class="{ active: eventTab === EventTab.HOSTS }">
+            <router-link :to="`?tab=${EventTab.HOSTS}`"> Hosts </router-link>
+          </li>
+
+          <li :class="{ active: eventTab === EventTab.QUESTIONS }">
+            <router-link :to="`?tab=${EventTab.QUESTIONS}`"> Questions </router-link>
+          </li>
+
+          <li :class="{ active: eventTab === EventTab.PUBLISH }">
+            <router-link :to="`?tab=${EventTab.PUBLISH}`"> Publish </router-link>
+          </li>
+        </ul>
       </div>
-    </main>
+    </div>
+
+    <loader :loading="event === null" class="w-full">
+      <hosts v-if="eventTab === EventTab.HOSTS" v-model="event" />
+      <publish v-if="eventTab === EventTab.PUBLISH" :event="event" />
+      <settings v-if="eventTab === EventTab.SETTINGS" v-model="event" />
+      <questions v-if="eventTab === EventTab.QUESTIONS" v-model="event" />
+    </loader>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
-import { dashboardStore } from '~/store';
+import { Component, Vue, Watch } from 'nuxt-property-decorator';
+
+import _ from 'lodash';
+
+import { Event } from '~/types/models/event';
+
+import Hosts from '~/components/event-manager/Hosts.vue';
+import Publish from '~/components/event-manager/Publish.vue';
+import Settings from '~/components/event-manager/Settings.vue';
+import Questions from '~/components/event-manager/Questions.vue';
+import ProgressButton from '~/components/common/ProgressButton.vue';
+
+enum EventTab {
+  HOSTS = 'hosts',
+  PUBLISH = 'publish',
+  SETTINGS = 'settings',
+  QUESTIONS = 'questions',
+}
 
 @Component({
   layout: 'dashboard',
   middleware: ['auth'],
+  components: { ProgressButton, Hosts, Publish, Settings, Questions },
 })
-export default class Modify extends Vue {
-  created() {
-    dashboardStore.setPageName('Dashboard');
+export default class ModifyEvent extends Vue {
+  EventTab = EventTab;
+
+  event: Event | null = null;
+  eventTab: EventTab | null = null;
+
+  async created() {
+    try {
+      const response = await this.$axios.get(
+        `${process.env.NUXT_ENV_API_ROUTE}/events/${this.$route.params.id}`,
+      );
+
+      this.event = response.data.data;
+
+      // If the event is not a draft, the questions cannot be modified
+      // eslint-disable-next-line camelcase
+      if (!this.event?.is_draft) {
+        return this.$router.push('/dashboard/events');
+      }
+    } catch {
+      // todo: show error message
+    }
+  }
+
+  @Watch('$route', { immediate: true })
+  handleRouteChanged(route: any) {
+    this.eventTab = route.query.tab || EventTab.SETTINGS;
+  }
+
+  get truncatedEventName() {
+    return _.truncate(this.event?.name || '', { length: 30 });
   }
 }
 </script>
