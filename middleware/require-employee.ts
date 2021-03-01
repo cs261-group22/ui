f1c94ec2af560089ea9cd3698f9e2c6f1830c361
Callum Bloomfield <callum.bloomfield01@gmail.com>
2021-03-01 00:49:24 +0000

    Fix require-employee middleware

--- middleware/require-employee.ts
+++ middleware/require-employee.ts
@@ -1,7 +1,7 @@
 import { Middleware } from '@nuxt/types';
 
 const requireProfileComplete: Middleware = (context) => {
-  if (context.$auth.user.data.is_guest) {
+  if (context.$auth?.user?.data?.is_guest) {
     context.$auth.logout();
     return context.redirect('/sign-in?target=dashboard');
   }
